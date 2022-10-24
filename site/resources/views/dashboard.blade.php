730034e5c35853f20e433a0e15b91c90a857db35
AdrianoMiguell <101981502+AdrianoMiguell@users.noreply.github.com>
2022-10-24 17:51:50 -0300

    aula4

--- site/resources/views/dashboard.blade.php
+++ site/resources/views/dashboard.blade.php
@@ -2,4 +2,16 @@
 
 @section('content')
 
+    @forelse ($notes as $key => $note)
+        <div class="card border border-2 shadow p-3" style="background-color: {{$user->color}}">
+            <div class="card-header"> {{$note->title}}</div>
+            <div class="card-body"> {{$note->content}} </div>
+        </div>
+        @empty
+        <div class="alert alert-danger">
+            Nenhuma anotação cadastrada!
+        </div>
+    @endforelse
+
+
 @endsection
